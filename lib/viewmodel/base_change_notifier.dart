d8edf0b060d6544d2853357209d2c31bc5465b23
vincent <vincent@360kredi.id>
2023-02-27 11:26:36 +0800

    处理 BottomNavigationBar' is not a subtype of type '(BuildContext, ChangeNotifier, Widget?) => Widget

--- lib/viewmodel/base_change_notifier.dart
+++ lib/viewmodel/base_change_notifier.dart
@@ -1,27 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
-// class BaseChangNotifier extends ChangeNotifier {
-//   // 页面销毁则不发送通知
-//   bool _dispose = false;
-//
-//   @override
-//   void dispose() {
-//     super.dispose();
-//     _dispose = true;
-//   }
-//
-//   // 报错：_debugAssertNotDisposed() --》
-//   // https://github.com/rrousselGit/provider/issues/78
-//   @override
-//   void notifyListeners() {
-//     if (!_dispose) {
-//       super.notifyListeners();
-//     }
-//   }
-// }
-
-class BaseChangeNotifier with ChangeNotifier {
-
+class BaseChangeNotifier extends ChangeNotifier {
+  // 页面销毁则不发送通知
   bool _dispose = false;
 
   @override
@@ -30,6 +10,8 @@ class BaseChangeNotifier with ChangeNotifier {
     _dispose = true;
   }
 
+  // 报错：_debugAssertNotDisposed() --》
+  // https://github.com/rrousselGit/provider/issues/78
   @override
   void notifyListeners() {
     if (!_dispose) {
